2747a8a5b623e395b7589fc1218115cf68fb2b5a
afreen shaik <affushaik9493@gmail.com>
2023-08-03 18:31:55 +0530

    added functionality

---
+++ src/components/profiles/profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './profile.module.css'
+const Profile = ()=> {
+  const missions = useSelector((state) => state.missions.missions);
+  const joinedMissions = useSelector((state) => state.missions.joinedMissions);
+  
+
+  const myJoinedMissions = missions.filter((mission) => joinedMissions.includes(mission.mission_id));
+  return (
+    <section className="profile-container">
+      <div className="column">
+        <h2 className="column-head">My Missions</h2>
+        {!myJoinedMissions.length ? (
+          <p className="empty-message">No joined missions.</p>
+        ) : (
+          <ul className="u-list">
+            {myJoinedMissions.map((mission) => (
+              <li className="profile-list" key={mission.mission_id}>
+                <h4 className="my-missions-name">{mission.mission_name}</h4>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+    </section>
+  );
+};
+
+
+export default Profile;
